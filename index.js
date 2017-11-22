afb46711b0338ce0425e20f07925ada665355334
Matthew Clinton <clintonmatthew@hotmail.com>
2017-11-22 16:18:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ function updateObjectWithKeyAndValue(object, key, value)
 {
   return Object.assign({}, object, { [key]: value })
 }
+//updates `object` with the given `key` and `value` (it is destructive) and returns the entire updated object
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value)
+{
+  object[key] = value
+
+  return object
+}
